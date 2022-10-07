71498c3f0d13f7a747fd4185a90e4aa973835bf7
guilhem <guilhem.bonnetjalabert@gmail.com>
2022-10-07 16:37:41 +0200

    modification d'animation d'UI avec LeanTween

--- Assets/BlockTheDeliveryMan/Scripts/AnimController/AnimPanelOption.cs
+++ Assets/BlockTheDeliveryMan/Scripts/AnimController/AnimPanelOption.cs
@@ -6,20 +6,53 @@ public class AnimPanelOption : MonoBehaviour
 {
     [SerializeField] private GameObject ButtonOpenOption;
     [SerializeField] private GameObject Panel;
+    [SerializeField] private Vector3 open;
+    [SerializeField] private Vector3 close;
+    [SerializeField] private float duration = 2;
+    private bool callClose = false;
+    private bool callOpen = false;
     public void OpenPanel()
     {
-        Panel.SetActive(true);
-        Panel.GetComponent<Animator>().enabled = true;
-        Panel.GetComponent<Animator>().SetBool("Open",true);
+        if (!callClose && !callOpen)
+        {
+            callOpen = true;
+            Panel.SetActive(true);
+            //Panel.GetComponent<RectTransform>().rect.;
+            LeanTween.move(Panel.GetComponent<RectTransform>(), Panel.GetComponent<RectTransform>().anchoredPosition3D + open, duration).setEase(LeanTweenType.easeOutQuad).setDelay(0.1f); // Déplacement
+            StartCoroutine(OpenPanel_IEnum());
+        }
+       
     }
+    private IEnumerator OpenPanel_IEnum()
+    {
+ 
+        yield return new WaitForSeconds(duration);
+        callOpen = false;
+        StopCoroutine(OpenPanel_IEnum());
+    }
+
+
     public void ClosePanel()
     {
-        Panel.GetComponent<Animator>().SetBool("Open", false);
-        // l'animator se charge de désactiver le panel
-    }
+        if (!callOpen & !callClose)
+        {
+            callClose = true;
+            LeanTween.move(Panel.GetComponent<RectTransform>(), Panel.GetComponent<RectTransform>().anchoredPosition3D + close, duration).setEase(LeanTweenType.easeOutQuad).setDelay(0); // Déplacement
+            StartCoroutine(ClosePanel_IEnum());
 
-    private void BackMenu()
+        }
+         
+    }
+    private IEnumerator ClosePanel_IEnum()
     {
+        //Vérifie que le panel est bien fermé avant de désactiver l'élément
+
+        yield return new WaitForSeconds(duration);
+        Panel.SetActive(false);
+        callClose = false;
+
+        StopCoroutine(ClosePanel_IEnum());
 
     }
+
 }
